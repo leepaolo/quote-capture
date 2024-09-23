e35eacf98c5ea16ecbb325a483d49fde63cc49ed
leepaolo <sottilestudio@gmail.com>
2024-09-23 17:24:48 +0200

    Fix(CREATE NEW QUOTE): Added the form group and edited the input-text component

--- src/app/components/shared/create-new-quote-bar/create-new-quote-bar.component.ts
+++ src/app/components/shared/create-new-quote-bar/create-new-quote-bar.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { InputTextComponent } from '../../parts/input-text.component';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-create-new-quote-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, InputTextComponent, ReactiveFormsModule],
   templateUrl: './create-new-quote-bar.component.html',
-  styleUrl: './create-new-quote-bar.component.css'
+  styleUrls: ['./create-new-quote-bar.component.css'], // Corrected 'styleUrls'
 })
-export class CreateNewQuoteBarComponent {
+export class CreateNewQuoteBarComponent implements OnInit, OnDestroy {
+  quoteText: string = 'Enter your quote';
+  authorText: string = 'Add the Author';
+  buttonText: string = 'Save';
 
+  private destroy$ = new Subscription();
+
+  createQuoteForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createQuoteForm = this.fb.group({
+      quoteText: ['', Validators.required],
+      authorText: ['', Validators.required],
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.unsubscribe();
+  }
+
+  onSubmit(): void {
+    if (this.createQuoteForm.valid) {
+      const quote = this.createQuoteForm.get('quoteText')?.value;
+      const author = this.createQuoteForm.get('authorText')?.value;
+      console.log('Form submitted:', { quote, author });
+      // You can handle form submission here, e.g., send data to a service
+    } else {
+      // Handle invalid form state
+      console.error('Form is invalid');
+    }
+  }
 }
